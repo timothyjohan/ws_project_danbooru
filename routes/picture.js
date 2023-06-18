7ffe21499d4d2422e037f966c1c9b35ed1cedce9
AlvinOeij <alvinoeij@gmail.com>
2023-06-18 09:25:36 +0700

    mengakses danboru
    sdh bisa ambil link dari danborunya + sdh ada sedikit informasi terkait danboru + sdh bs ngesearch kata pertama (hanya menampilkan 20 data saja)

--- routes/picture.js
+++ routes/picture.js
@@ -6,11 +6,52 @@ const Joi = require('joi');
 const axios = require('axios');
 
 router.get('/', async (req, res) => {
-    let link = "https://cdn.donmai.us/";
-    let pict = "sample/f9/08/__tingyun_honkai_and_1_more_drawn_by_nanni_jjang__sample-f908fbf975c2b5e7f5d7086a6c6f123d.jpg";
-    let getdata = await axios.get(`https://cdn.donmai.us/${pict}`);
+    axios.get(`https://danbooru.donmai.us/posts.json`).then(response => {
+        console.log(response.data.id[response.count]);
+    })
+})
 
-    res.status(200).send(getdata)
+router.get('/info', async (req, res) => {
+    let num = req.query.id;
+    axios.get(`https://danbooru.donmai.us/posts/${num}.json`)
+    .then(response => {
+        console.log(response.data);
+        res.status(200).send(
+            {
+                id : response.data.id,
+                tag_name : response.data.tag_string_character,
+                created_at : response.data.created_at,
+                score : response.data.score,
+                fav_count : response.data.fav_count,
+                rating : response.data.rating,
+                picture : response.data.file_url,
+                width : response.data.media_asset.image_width +"px",
+                height : response.data.media_asset.image_height +"px",
+            });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(404).send({Error : 'Posts not found!'});
+    });
+})
+
+router.get('/search', async (req, res) => {
+    let start = req.query.start_with;
+    let search = await axios.get (`https://danbooru.donmai.us/tags.json?search[name_matches]=${start}*`)
+    let count  = (search.data).length;
+    let data = [];
+    for (let i = 0; i < count; i++) {
+        data.push({
+            id : search.data[i].id,
+            name : search.data[i].name,
+            created_at : search.data[i].created_at,
+            updated_at : search.data[i].updated_at,
+        })
+    }
+    res.status(200).send({
+        Note : `Here is 20 result about name tag start with ${start}`,
+        Data :data
+    })
 })
 
 module.exports = router;
