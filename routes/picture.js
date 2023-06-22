31d29052adb7e03381737891845964828f5d5d82
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2023-06-22 23:28:04 +0700

    Update picture.js

--- routes/picture.js
+++ routes/picture.js
@@ -4,47 +4,47 @@ const jwt = require("jsonwebtoken");
 const JWT_KEY = "UvuvwevwevweOnyetenyevweUgwemubwemOssas";
 const Joi = require("joi");
 const axios = require("axios");
-const Users = require('../models/Users');
-const Favorite = require('../models/Favorite');
+const Users = require("../models/Users");
+const Favorite = require("../models/Favorite");
 const { restart } = require("nodemon");
 
 //MASUKIN POST KE FAV
 router.post("/fav", async (req, res) => {
     ////////////////////////////////////////////////////////// JOI
     const schema = Joi.object({
-        id : Joi.string().required(),
+        id: Joi.string().required(),
     });
 
     try {
-        await schema.validateAsync(req.body)
+        await schema.validateAsync(req.body);
     } catch (error) {
-        return res.status(403).send(error.toString())
+        return res.status(403).send(error.toString());
     }
     ////////////////////////////////////////////////////////// AUTH
-    let token = req.header('x-auth-token')
-    if (!req.header('x-auth-token')) {
-        let out = 'Authentication token is missing';
+    let token = req.header("x-auth-token");
+    if (!req.header("x-auth-token")) {
+        let out = "Authentication token is missing";
         return res.status(401).send({ out });
     }
-    let username
+    let username;
     try {
         let userdata = jwt.verify(token, JWT_KEY);
         console.log(userdata.username);
-        username = userdata.username
+        username = userdata.username;
     } catch (err) {
-        return res.status(400).send('Invalid JWT Key');
+        return res.status(400).send("Invalid JWT Key");
     }
 
     ////////////////////////////////////////////////////////// SEARCH ID FROM DANBORU
     let num = req.body.id;
-    let data
-    let response
+    let data;
+    let response;
     try {
-        response = await axios.get(`https://danbooru.donmai.us/posts/${num}.json`)
-
+        response = await axios.get(
+            `https://danbooru.donmai.us/posts/${num}.json`
+        );
     } catch (error) {
-        return res.status(404).send('Post not found');
-
+        return res.status(404).send("Post not found");
     }
     // console.log(response.data);
     data = {
@@ -53,29 +53,28 @@ router.post("/fav", async (req, res) => {
         artist: response.data.tag_string_artist,
         fav_count: response.data.fav_count,
         picture: response.data.file_url,
-        tags: response.data.tag_string
-    }
+        tags: response.data.tag_string,
+    };
     // return res.status(200).send(data)
-    console.log(`data is : ${data}`)
+    console.log(`data is : ${data}`);
 
+    let tempID = data.id;
+    let tempTagname = data.tag_name;
+    let tempartist = data.artist;
+    let tempPic = data.picture;
+    let tempTags = data.tags;
 
-    let tempID = data.id
-    let tempTagname = data.tag_name
-    let tempartist = data.artist
-    let tempPic = data.picture
-    let tempTags = data.tags
-
-    let query
+    let query;
 
     try {
         query = await Favorite.create({
             us_username: username,
-            id_picture: tempID
-        })
+            id_picture: tempID,
+        });
     } catch (error) {
         return res.status(500).send(error);
     }
-    let message = `Post has been added to your favorite successfully`
+    let message = `Post has been added to your favorite successfully`;
 
     return res.status(201).send({
         message: message,
@@ -83,38 +82,36 @@ router.post("/fav", async (req, res) => {
         tag_name: tempTagname,
         artist: tempartist,
         picture: tempPic,
-        tags: tempTags
-    })
-
-
+        tags: tempTags,
+    });
 });
 
 //SHOW POST YANG SDH MASUK FAV
 router.get("/fav/show", async (req, res) => {
     //////////////////////////////////////////////////////////
-    let token = req.header('x-auth-token')
-    if (!req.header('x-auth-token')) {
-        let out = 'Authentication token is missing';
+    let token = req.header("x-auth-token");
+    if (!req.header("x-auth-token")) {
+        let out = "Authentication token is missing";
         return res.status(401).send({ out });
     }
-    let username
-    let userdata
+    let username;
+    let userdata;
     try {
         userdata = jwt.verify(token, JWT_KEY);
         console.log(userdata.username);
-        username = userdata.username
+        username = userdata.username;
     } catch (err) {
-        return res.status(400).send('Invalid JWT Key');
+        return res.status(400).send("Invalid JWT Key");
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     let getFav = await Favorite.findAll({
-        where:{
-            us_username : username
+        where: {
+            us_username: username,
         },
-        attributes: ['id_picture']
-    })
+        attributes: ["id_picture"],
+    });
     console.log(getFav);
     return res.status(201).send(getFav);
 });
@@ -122,96 +119,102 @@ router.get("/fav/show", async (req, res) => {
 //DELETE POST
 router.delete("/delete", async (req, res) => {
     const schema = Joi.object({
-        id: Joi.string().required()
+        id: Joi.string().required(),
     });
 
     try {
-        await schema.validateAsync(req.body)
+        await schema.validateAsync(req.body);
     } catch (error) {
-        return res.status(403).send(error.toString())
+        return res.status(403).send(error.toString());
     }
-    
+
     //////////////////////////////////////////////////////////
-    let token = req.header('x-auth-token')
-    if (!req.header('x-auth-token')) {
-        let out = 'Authentication token is missing';
+    let token = req.header("x-auth-token");
+    if (!req.header("x-auth-token")) {
+        let out = "Authentication token is missing";
         return res.status(401).send({ out });
     }
-    let username
-    let userdata
+    let username;
+    let userdata;
     try {
         userdata = jwt.verify(token, JWT_KEY);
         console.log(userdata.username);
-        username = userdata.username
+        username = userdata.username;
     } catch (err) {
-        return res.status(400).send('Invalid JWT Key');
+        return res.status(400).send("Invalid JWT Key");
     }
 
     const num = req.body.id;
     let findFav = await Favorite.findOne({
-        where:{
-            us_username : username,
-            id_picture : num
-        }
-    })
-    if(findFav){
-        let delUser
+        where: {
+            us_username: username,
+            id_picture: num,
+        },
+    });
+    if (findFav) {
+        let delUser;
         try {
             delUser = await Favorite.destroy({
                 where: {
                     us_username: username,
-                    id_picture : num
-                }
-            })
+                    id_picture: num,
+                },
+            });
         } catch (error) {
-            return res.status(500).send(error)
+            return res.status(500).send(error);
         }
-        return res.status(201).send({ message: 'Post has been deleted from the database' })
-    }else{
-        return res.status(404).send({ message: `Post not found in favorite`})
+        return res
+            .status(201)
+            .send({ message: "Post has been deleted from the database" });
+    } else {
+        return res.status(404).send({ message: `Post not found in favorite` });
     }
 });
 
 //Search by ID
 router.get("/info", async (req, res) => {
     const schema = Joi.object({
-        id: Joi.string().required()
+        id: Joi.string().required(),
     });
 
     try {
-        await schema.validateAsync(req.query)
+        await schema.validateAsync(req.query);
     } catch (error) {
-        return res.status(403).send(error.toString())
+        return res.status(403).send(error.toString());
     }
-    const data = {...req.query}
+    const data = { ...req.query };
     //////////////////////////////////////////////////////////
-    let token = req.header('x-auth-token')
-    if (!req.header('x-auth-token')) {
-        let out = 'Authentication token is missing';
+    let token = req.header("x-auth-token");
+    if (!req.header("x-auth-token")) {
+        let out = "Authentication token is missing";
         return res.status(401).send({ out });
     }
-    let username
-    let userdata
+    let username;
+    let userdata;
     try {
         userdata = jwt.verify(token, JWT_KEY);
         console.log(userdata.username);
-        username = userdata.username
+        username = userdata.username;
     } catch (err) {
-        return res.status(400).send('Invalid JWT Key');
+        return res.status(400).send("Invalid JWT Key");
     }
 
     //////////////////////////////////////////////////////////
 
     //Payment kuota
-    let findUser = await Users.findByPk(username)
+    let findUser = await Users.findByPk(username);
     if (findUser.us_kuota < 3) {
-        return res.status(403).send('Kuota tidak mencukupi, untuk melakukan fitur ini diperlukan 3 kuota')
+        return res
+            .status(403)
+            .send(
+                "Kuota tidak mencukupi, untuk melakukan fitur ini diperlukan 3 kuota"
+            );
     }
-    let tempKuota = findUser.us_kuota - 3
+    let tempKuota = findUser.us_kuota - 3;
     let update = await Users.update(
         { us_kuota: tempKuota },
         { where: { us_username: username } }
-    )
+    );
 
     let num = req.query.id;
     axios
@@ -230,7 +233,7 @@ router.get("/info", async (req, res) => {
                 width: response.data.media_asset.image_width + "px",
                 height: response.data.media_asset.image_height + "px",
                 tags: response.data.tag_string,
-                kuota_user: tempKuota
+                kuota_user: tempKuota,
             });
         })
         .catch((error) => {
@@ -242,52 +245,54 @@ router.get("/info", async (req, res) => {
 // Search by tags
 router.get("/search", async (req, res) => {
     //////////////////////////////////////////////////////////
-    let token = req.header('x-auth-token')
-    if (!req.header('x-auth-token')) {
-        let out = 'Authentication token is missing';
+    let token = req.header("x-auth-token");
+    if (!req.header("x-auth-token")) {
+        let out = "Authentication token is missing";
         return res.status(401).send({ out });
     }
-    let username
-    let userdata
+    let username;
+    let userdata;
     try {
         userdata = jwt.verify(token, JWT_KEY);
         console.log(userdata.username);
-        username = userdata.username
-        // const insert = 
+        username = userdata.username;
+        // const insert =
     } catch (err) {
-        return res.status(400).send('Invalid JWT Key');
+        return res.status(400).send("Invalid JWT Key");
     }
 
     //////////////////////////////////////////////////////////
 
-
     //Payment kuota
-    let findUser = await Users.findByPk(username)
+    let findUser = await Users.findByPk(username);
     if (findUser.us_kuota < 5) {
-        return res.status(403).send('Kuota tidak mencukupi, untuk melakukan fitur ini diperlukan 5 kuota')
+        return res
+            .status(403)
+            .send(
+                "Kuota tidak mencukupi, untuk melakukan fitur ini diperlukan 5 kuota"
+            );
     }
-    let tempKuota = findUser.us_kuota - 5
+    let tempKuota = findUser.us_kuota - 5;
     let update = await Users.update(
         { us_kuota: tempKuota },
         { where: { us_username: username } }
-    )
-
+    );
 
     let start = req.query.start_with;
-    let nsfw = ''
+    let nsfw = "";
     if (req.query.nsfw) {
         // return res.status(200).send('yes')
-        nsfw = 'is%3Asfw'
-        if (req.query.nsfw == 'true') {
-            console.log(req.query.nsfw)
-            nsfw = 'is%3ansfw'
+        nsfw = "is%3Asfw";
+        if (req.query.nsfw == "true") {
+            console.log(req.query.nsfw);
+            nsfw = "is%3ansfw";
         }
     }
     let search = await axios.get(
         `https://danbooru.donmai.us/posts.json?tags=${start}+${nsfw}`
     );
     // console.log(search)
-    let temp = search.data
+    let temp = search.data;
 
     let count = search.data.length;
     let data = [];
@@ -309,45 +314,43 @@ router.get("/search", async (req, res) => {
 // Get all post from latest
 router.get("/search_notag", async (req, res) => {
     //////////////////////////////////////////////////////////
-    let token = req.header('x-auth-token')
-    if (!req.header('x-auth-token')) {
-        let out = 'Authentication token is missing';
+    let token = req.header("x-auth-token");
+    if (!req.header("x-auth-token")) {
+        let out = "Authentication token is missing";
         return res.status(401).send({ out });
     }
-    let username
-    let userdata
+    let username;
+    let userdata;
     try {
         userdata = jwt.verify(token, JWT_KEY);
         console.log(userdata.username);
-        username = userdata.username
-        // const insert = 
+        username = userdata.username;
+        // const insert =
     } catch (err) {
-        return res.status(400).send('Invalid JWT Key');
+        return res.status(400).send("Invalid JWT Key");
     }
 
     //////////////////////////////////////////////////////////
 
-
     //Payment kuota
-    let findUser = await Users.findByPk(username)
+    let findUser = await Users.findByPk(username);
     if (findUser.us_kuota < 10) {
-        return res.status(403).send('Kuota tidak mencukupi, untuk melakukan fitur ini diperlukan 10 kuota')
+        return res
+            .status(403)
+            .send(
+                "Kuota tidak mencukupi, untuk melakukan fitur ini diperlukan 10 kuota"
+            );
     }
-    let tempKuota = findUser.us_kuota - 10
+    let tempKuota = findUser.us_kuota - 10;
     let update = await Users.update(
         { us_kuota: tempKuota },
         { where: { us_username: username } }
-    )
-
-
-
+    );
 
     let start = req.query.start_with;
-    let search = await axios.get(
-        `https://danbooru.donmai.us/posts.json`
-    );
-    console.log(search)
-    let temp = search
+    let search = await axios.get(`https://danbooru.donmai.us/posts.json`);
+    console.log(search);
+    let temp = search;
     // return res.status(200).send(temp)
 
     let count = search.data.length;
@@ -367,4 +370,39 @@ router.get("/search_notag", async (req, res) => {
     });
 });
 
+router.get("/random-image", async (req, res) => {
+    try {
+        const min = 100000; // Nilai minimum (inklusif)
+        const max = 999999; // Nilai maksimum (inklusif)
+        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        const response = await axios.get(
+            `https://danbooru.donmai.us/posts/${randomNum}.json`
+        );
+        console.log(randomNum);
+        const {
+            id,
+            file_url,
+            tag_string,
+            description,
+            image_width,
+            image_height,
+            artist,
+        } = response.data;
+
+        const tags = tag_string.split(" ");
+        const imageData = {
+            id,
+            url: file_url,
+            artist: artist ? artist : "Unknown",
+            tags,
+            description: description ? description : "",
+            width: image_width,
+            height: image_height,
+        };
+        res.json(imageData);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 module.exports = router;
